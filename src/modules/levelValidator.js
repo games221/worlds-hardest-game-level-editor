54023a530039e98bf0e9bd9cb3da8388629eec8d
games221 <crisgamingzone@gmail.com>
2026-05-15 09:49:11 +0100

    Add levelValidator module

---
+++ src/modules/levelValidator.js
@@ -0,0 +1,12 @@
+// levelValidator.js
+// Valida que el nivel sea correcto.
+
+// TODO:
+// - validateLevel(state)
+
+export function validateLevel(/* state */) {
+  return {
+    valid: true,
+    errors: []
+  };
+}
